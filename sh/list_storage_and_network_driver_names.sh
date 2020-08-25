ac2b114a902c940a75da3b60fd51023cebe9e3fe
Yuval LB <yuval@lightsrv5.lbits>
2020-08-25 12:16:03 +0300

    allow make fat inaugurator version
    create fat inaugurator

--- sh/list_storage_and_network_driver_names.sh
+++ sh/list_storage_and_network_driver_names.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #list the drivers for the fat version
-BLACKLIST='-e bfa -e csiostor -e cxgb4 -e bna -e aic94xx'
+BLACKLIST='-e bfa -e csiostor -e cxgb4 -e bna -e aic94xx -e mlx4_core -e mlx4_en -e cxgb3'
 KERNEL_VERSION=$1
 set -e
 find /lib/modules/$KERNEL_VERSION/kernel/drivers/net/ethernet /lib/modules/$KERNEL_VERSION/kernel/drivers/scsi -type f -printf '%f\n' | sed 's/\.ko.xz$//' | grep -v $BLACKLIST
